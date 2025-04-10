35cc274bb23d33d31d8a0759b93f444d209ac449
Fabricio Valdez <fabriciovaldezz41@gmail.com>
2025-04-10 08:44:06 -0300

    Edit 1

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from naive_bayes.training import entrenar_naive_bayes
 from naive_bayes.sorter import clasificar
 from naive_bayes.preprocessing import cargar_datos
 
-# Entrenar modelo
+# Training
 textos = cargar_datos()
 probs_clase, probs_palabra_dado_clase, vocabulario = entrenar_naive_bayes(textos)
 
-# Modo interactivo
+# Input
 print("=== Clasificador de SPAM ===")
 print("Escribe un mensaje y te diré si es SPAM o NO SPAM.")
 print("Escribe 'salir' para terminar.\n")
